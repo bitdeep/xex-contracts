4135094ecfa93bbb93ebfbd176ad087c498a21b5
bitdeep <19378023+bitdeep@users.noreply.github.com>
2022-11-30 14:58:00 -0300

    setTrustedRemoteAddress

--- scripts/test.js
+++ scripts/test.js
@@ -7,20 +7,31 @@ module.exports = async function(callback) {
         const main = await Main.deployed()
         console.log('main', main.address)
         const fee = (await main.fee()).toString();
-        console.log('fee', fee);
+
 
         let adapterParams = ethers.utils.solidityPack(
             ["uint16", "uint", "uint", "address"],
             [2, 200000, 0, accounts[0]]
         );
-        let LzCallParams = ethers.utils.solidityPack(
-            ["address", "address", "bytes"],
-            [accounts[0], accounts[0], adapterParams]
-        )
+        let LzCallParams = {
+            refundAddress: accounts[0],
+            zroPaymentAddress: accounts[0],
+            adapterParams: []
+        }
 
-        const OneEther = '1000000000000000000';
-        const fees = await main.estimateSendFee('10102', accounts[0], OneEther, false, LzCallParams);
-        console.log('fees', fees);
+        const OneEther = '100000000000000000';
+        const fees = await main.estimateSendFee('10102', accounts[0], OneEther, false, adapterParams);
+        const nativeFee = fees.nativeFee.toString();
+
+        const dst = '10102';
+        console.log('mint fee', fee, 'send fee', nativeFee);
+
+        // mumbai from fuji
+        // await main.setTrustedRemoteAddress('10106', '0x35fb4895b68aC7A2D11da59d4E9f520c9C86A546');
+
+        // fuji to mumbai
+        await main.setTrustedRemoteAddress(dst, '0xd3398d9038A57f503f14a1F6eb463292b1caDDa6');
+        await main.sendFrom(accounts[0], dst, accounts[0], OneEther, LzCallParams, {from: accounts[0], value: nativeFee});
 
     }
     catch(error) {
